4be8d61f5b2f22c89c54d53ba71a4018516e771e
romeluis <113552563+romeluis@users.noreply.github.com>
2024-01-17 14:28:41 -0500

    added pa 1

---
+++ PA1/starter_code/LinearRegressionImp.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+
+
+def fit_LinRegr(X_train, y_train):
+    #Add implementation here
+
+def mse(X_train,y_train,w):
+    #Add implementation here
+
+def pred(X_train,w):
+    #Add implementation here
+
+def test_SciKit(X_train, X_test, Y_train, Y_test):
+    #Add implementation here
+
+def subtestFn():
+    # This function tests if your solution is robust against singular matrix
+
+    # X_train has two perfectly correlated features
+    X_train = np.asarray([[1, 2], [2, 4], [3, 6], [4, 8]])
+    y_train = np.asarray([1,2,3,4])
+    
+    try:
+      w=fit_LinRegr(X_train, y_train)
+      print ("weights: ", w)
+      print ("NO ERROR")
+    except:
+      print ("ERROR")
+
+def testFn_Part2():
+    X_train, y_train = load_diabetes(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X_train,y_train,test_size=0.2)
+    
+    w=fit_LinRegr(X_train, y_train)
+    
+    #Testing Part 2a
+    e=mse(X_test,y_test,w)
+    
+    #Testing Part 2b
+    scikit=test_SciKit(X_train, X_test, y_train, y_test)
+    
+    print("Mean squared error from Part 2a is ", e)
+    print("Mean squared error from Part 2b is ", scikit)
+
+print ('------------------subtestFn----------------------')
+subtestFn()
+
+print ('------------------testFn_Part2-------------------')
+testFn_Part2()
---
+++ PA1/starter_code/PerceptronImp.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix 
+
+def fit_perceptron(X_train, y_train):
+    #Add implementation here 
+
+def errorPer(X_train,y_train,w):
+    #Add implementation here 
+
+def confMatrix(X_train,y_train,w):
+    #Add implementation here 
+
+def pred(X_train,w):
+    #Add implementation here
+
+def test_SciKit(X_train, X_test, Y_train, Y_test):
+    #Add implementation here 
+
+def test_Part1():
+    from sklearn.datasets import load_iris
+    X_train, y_train = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X_train[50:],y_train[50:],test_size=0.2)
+
+    #Set the labels to +1 and -1
+    y_train[y_train == 1] = 1
+    y_train[y_train != 1] = -1
+    y_test[y_test == 1] = 1
+    y_test[y_test != 1] = -1
+
+    #Pocket algorithm using Numpy
+    w=fit_perceptron(X_train,y_train)
+    cM=confMatrix(X_test,y_test,w)
+
+    #Pocket algorithm using scikit-learn
+    sciKit=test_SciKit(X_train, X_test, y_train, y_test)
+    
+    #Print the result
+    print ('--------------Test Result-------------------')
+    print("Confusion Matrix is from Part 1a is: ",cM)
+    print("Confusion Matrix from Part 1b is:",sciKit)
+    
+
+test_Part1()
