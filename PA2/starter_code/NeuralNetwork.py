7bea59212eb10fc8d3d401691bff46cbc91b4cd6
Shivansh <shivanshsingh@Shivanshs-MacBook-Air.local>
2024-02-27 14:44:19 -0500

    Finished part 1

--- PA2/starter_code/NeuralNetwork.py
+++ PA2/starter_code/NeuralNetwork.py
@@ -10,48 +10,69 @@ from sklearn.metrics import confusion_matrix
 
 def fit_NeuralNetwork(X_train,y_train,alpha,hidden_layer_sizes,epochs):
     #Enter implementation here
+    pass
     
 def forwardPropagation(x, weights):
     #Enter implementation here
+    pass
 
 def errorPerSample(X,y_n):
     #Enter implementation here
+    pass
 
 def backPropagation(X,y_n,s,weights):
     #Enter implementation here
+    pass
 
 def updateWeights(weights,g,alpha):
     #Enter implementation here
+    pass
 
 def activation(s):
     #Enter implementation here
+    return s if s >= 0 else 0
 
 def derivativeActivation(s):
     #Enter implementation here
+    return 1 if s >= 0 else 0
 
 def outputf(s):
     #Enter implementation here
+    return 1 / (1 + np.exp(-1*s))
 
 def derivativeOutput(s):
     #Enter implementation here
+    return np.exp(-1*s) / ((1 + np.exp(-1*s)) ** 2)
 
 def errorf(x_L,y):
     #Enter implementation here
+    if y == 1:
+        return -1 * np.log(x_L)
+    else:
+        return -1 * np.log(1 - x_L)
 
 def derivativeError(x_L,y):
     #Enter implementation here
+    if y == 1:
+        return -1 / x_L
+    else:
+        return 1 / (1 - x_L)
 
 def pred(x_n,weights):
     #Enter implementation here
+    pass
     
 def confMatrix(X_train,y_train,w):
     #Enter implementation here
+    pass
 
 def plotErr(e,epochs):
     #Enter implementation here
+    pass
     
 def test_SciKit(X_train, X_test, Y_train, Y_test):
     #Enter implementation here
+    pass
 
 def test_Part1():
     from sklearn.datasets import load_iris
